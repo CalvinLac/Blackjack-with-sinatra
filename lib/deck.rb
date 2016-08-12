bb0e3d2e2ee0498a21aff731458889fea6b51c29
CalvinLac <Calvin.lacc@gmail.com>
2016-08-11 20:28:44 -0600

    Refactoring

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,9 +1,11 @@
 class Deck
-
+  
+  #Forming the deck for all possible cards
   def initialize
     @possible_cards = [2,3,4,5,6,7,8,9,10,10,10,10,11]
   end
 
+  #Running the sample method to generate a random object from possible cards
   def random
     card_dealt = @possible_cards.sample
   end
